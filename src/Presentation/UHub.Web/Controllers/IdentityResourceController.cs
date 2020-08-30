13ecbf5e78eabf5bdc42d7ae18d7ca2c5487b8a4
yiyun <yiyungent@126.com>
2020-08-30 16:32:01 +0800

    feat: add IdentityResource list

---
+++ src/Presentation/UHub.Web/Controllers/IdentityResourceController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UHub.Web.Models.Client;
+using UHub.Web.Models.IdentityResource;
+
+namespace UHub.Web.Controllers
+{
+    [Authorize("Admin")]
+    public class IdentityResourceController : Controller
+    {
+        #region Fields
+
+        private readonly ConfigurationDbContext _configurationDbContext;
+
+        #endregion
+
+        #region Ctor
+
+        public IdentityResourceController(ConfigurationDbContext configurationDbContext)
+        {
+            _configurationDbContext = configurationDbContext;
+        }
+        #endregion
+
+        #region Actions
+
+        #region IdentityResource列表
+        public async Task<IActionResult> Index()
+        {
+            IList<IdentityResourceViewModel> viewModel = new List<IdentityResourceViewModel>();
+            var dbModel = await _configurationDbContext.IdentityResources
+                    .Include(d => d.UserClaims)
+                    .Include(d => d.Properties)
+                .ToListAsync();
+            foreach (var dbIdentityResource in dbModel)
+            {
+                var identityResourceModel = dbIdentityResource.ToModel();
+                viewModel.Add(new IdentityResourceViewModel
+                {
+                    Id = dbIdentityResource.Id,
+                    Name = identityResourceModel.Name,
+                    DisplayName = identityResourceModel.DisplayName,
+                    Description = identityResourceModel.Description,
+                    Required = identityResourceModel.Required,
+                    ShowInDiscoveryDocument = identityResourceModel.ShowInDiscoveryDocument,
+                    UserClaims = identityResourceModel.UserClaims,
+                    CreateTime = dbIdentityResource.Created.ToString("yyyy-MM-dd HH:mm"),
+                    LastUpdateTime = dbIdentityResource.Updated?.ToString("yyyy-MM-dd HH:mm") ?? dbIdentityResource.Created.ToString("yyyy-MM-dd HH:mm"),
+                });
+            }
+
+            return View(viewModel);
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ src/Presentation/UHub.Web/Models/IdentityResource/IdentityResourceViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UHub.Web.Models.IdentityResource
+{
+    public class IdentityResourceViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Required { get; set; }
+
+        public bool ShowInDiscoveryDocument { get; set; }
+
+        public ICollection<string> UserClaims { get; set; }
+
+        public string CreateTime { get; set; }
+
+        public string LastUpdateTime { get; set; }
+    }
+}
